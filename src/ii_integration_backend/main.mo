d56fdfa6a4ac309e5436f020d1cd4c0bf48939ec
Vedant-asati <vedant.dev.jsr@gmail.com>
2023-12-02 17:13:34 +0530

    jsr

--- src/ii_integration_backend/main.mo
+++ src/ii_integration_backend/main.mo
@@ -1,5 +1,52 @@
+import D "mo:base/Debug";
+import Float "mo:base/Float";
+import Nat "mo:base/Nat";
+import Time "mo:base/Time";
+import Int "mo:base/Int";
+// import Text "mo:base/Text";
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+    public shared (msg) func whoami() : async Principal {
+        msg.caller
+    };
+  // stable
+  stable var hih : Float = 214_214;
+  stable var startS : Float = 1700836000;
+  func greet(_name : Text) {
+    D.print(debug_show (hih));
+    D.print(debug_show (_name));
+    // return "Hello, " # name # "!";
+  };
+  public func inc(val : Float) {
+    hih := hih+val;
+    D.print(debug_show (hih));
+  };
+  public func decr(val : Float) : async Float {
+    if (val <= hih) {
+      hih := hih-val;
+      D.print(debug_show (hih));
+    } else D.print("JSR! Thats not gonna work!");
+    return hih;
+  };
+  public query func getBalance() : async Float {
+    // D.print(debug_show (hih));
+    return hih;
+  };
+  // let timeNow = Time.now();
+  public func getTime() : async Int {
+    let timeNow = Time.now();
+    D.print(debug_show (timeNow));
+    return timeNow;
+  };
+  public func compound() : async Float {
+    let timeNow = Float.fromInt(Time.now());
+    D.print(debug_show ("before : ", hih));
+    let elapsedS : Float = (timeNow / 1000000000) -startS;
+    D.print(debug_show ("startS : ", startS));
+    D.print(debug_show ("nowS : ", (timeNow / 1000000000)));
+    D.print(debug_show ("elapsedS : ", elapsedS));
+    hih := hih * (1.001 ** elapsedS);
+    D.print(debug_show ("after : ", hih));
+    startS := timeNow / 1000000000;
+    return hih;
   };
 };
--- src/ii_integration_frontend/src/index.js
+++ src/ii_integration_frontend/src/index.js
@@ -1,19 +1,75 @@
-import { ii_integration_backend } from "../../declarations/ii_integration_backend";
-
-document.querySelector("form").addEventListener("submit", async (e) => {
+import {
+  createActor,
+  ii_integration_backend,
+} from "../../declarations/ii_integration_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
+let actor = ii_integration_backend;
+console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
+const whoAmIButton = document.getElementById("whoAmI");
+whoAmIButton.onclick = async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await ii_integration_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  whoAmIButton.setAttribute("disabled", true);
+  const principal = await actor.whoami();
+  whoAmIButton.removeAttribute("disabled");
+  document.getElementById("principal").innerText = principal.toString();
   return false;
+};
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
+  e.preventDefault();
+  let authClient = await AuthClient.create();
+  // start the login process and wait for it to finish
+  await new Promise((resolve) => {
+      authClient.login({
+          identityProvider:
+              process.env.DFX_NETWORK === "ic"
+                  ? "https://identity.ic0.app"
+                  : `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai`,
+          onSuccess: resolve,
+      });
+  });
+  const identity = authClient.getIdentity();
+  const agent = new HttpAgent({ identity });
+  actor = createActor(process.env.CANISTER_ID_II_INTEGRATION_BACKEND, {
+      agent,
+  });
+  return false;
+};
+//////////////////
+
+let currentBalance=0.00;
+const updateBalance= async()=>{
+try {
+  console.log("jai siyaram...");
+  currentBalance=await actor.getBalance();
+  currentBalance=Math.round(currentBalance*100)/100;
+  document.getElementById("balance").innerHTML=currentBalance;
+  console.log("Updated balance");
+} catch (error) {
+  console.log(error);
+}
+}
+//
+window.addEventListener("load",()=>{
+  updateBalance();
 });
+let submitButton=document.getElementById("submit-btn");
+submitButton.addEventListener("click",async (e)=>{
+  e.preventDefault();
+  // console.log("j")
+  try {
+    console.log("jsr started");
+    submitButton.setAttribute("disabled",true);
+    const incValue =parseFloat(document.getElementById("input-amount").value);
+    const decrValue =parseFloat(document.getElementById("withdrawal-amount").value);
+    await actor.inc(incValue);
+    let a=await actor.decr(decrValue);
+    console.log("jsr done");
+    submitButton.removeAttribute("disabled");
+    await updateBalance();
+
+  } catch (error) {
+    console.log(error);
+  }
+})
